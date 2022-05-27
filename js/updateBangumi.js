0bda65fa9b00385cc6a2f14803cb84cef07e127b
zhangheng <986472954@qq.com>
2022-05-27 17:59:46 +0800

    ok

--- js/updateBangumi.js
+++ js/updateBangumi.js
@@ -1,113 +1,208 @@
 const axios = require('axios');
 var convert = require('xml-js');
 
-function updateBangumi(bot, connection){
-    let subscribes = []
-    //查询所有番剧
-    connection.query('SELECT * from bangumi', function (error, results, fields) {
-      if (error) throw error;
-      console.log("results1 ", results)
-      results.forEach(item => {
-        let title = item.title.split(',')
-        subscribes.push({
-            id: item.id,
-            title: title,
-            lastIndex: item.last_index,
-            source:[]
-        })
-      });
-      console.log("subscribes ", subscribes)
-    });
-
-    axios.get('https://mikanani.me/RSS/MyBangumi?token=g7fiCQpq5lEeUkZ3gSn27LgehEcZ3larcTuNwFb5LUw%3d')
-    .then(res => {
-        var result = convert.xml2json(res.data, {compact: true, spaces: 4});
-        let rss = JSON.parse(result)
-        let bangumi = rss.rss.channel.item;        
-        //填充订阅资源列表
-        subscribes.forEach((sub, index) => {
-            //可能存在多个名称，所以每个名称都匹配一遍
-            sub.title.forEach(name => {
-                bangumi.forEach(anim => {
-                    if(anim.guid._text.lastIndexOf(name) >= 0){
-                        //查询出这是第几集
-                        let episodeIndex = String(anim.guid._text).search(/\[[0-9][0-9]\]/) == -1 ? //[05]
-                                (String(anim.guid._text).search(/\【[0-9][0-9]\】/) == -1 ? //【05】
-                                    (String(anim.guid._text).search(/\s[0-9][0-9]\s/) == -1 ? //  05  
-                                    -1:String(anim.guid._text).search(/\s[0-9][0-9]\s/))
-                                :String(anim.guid._text).search(/\【[0-9][0-9]\】/) )
-                            :String(anim.guid._text).search(/\[[0-9][0-9]\]/)
-                        if(episodeIndex != -1){
-                            let episode = parseInt(anim.guid._text.slice(episodeIndex+1, episodeIndex+3))
-                            subscribes[index].source.push({
-                                title: anim.guid._text,
-                                episode: episode,
-                                link: anim.link._text,
-                                torrent: anim.enclosure._attributes.url
-                            })
-                            
-                        }
-                    }
-                });
+//更新资源
+function updateBangumi(connection){
+    return new Promise(function(resove, reject){
+        let subscribes = []
+        //查询该用户订阅的所有番剧
+        connection.query('SELECT * from bangumi', function (error, results, fields) {
+          if (error) throw error;
+          results.forEach(item => {
+            let title = item.title.split(',')
+            subscribes.push({
+                id: item.id,
+                title: title,
+                lastIndex: item.last_index,
+                source:[]
             })
-        })
-
-        //按集数倒序排列
-        subscribes.forEach(sub => {
-            sub.source.sort(function(a, b) {
-                return b.episode - a.episode
-            })
+          });
         });
-
-        //更新番剧资源
-        let newList = []
-        subscribes.forEach((sub, index) => {
-            let LastIndex = subscribes[index].lastIndex
-            sub.source.forEach(item => {
-                //判断该资源是否已被收录
-                let sql = `SELECT id from resources where link = '${item.link}'`
-                connection.query(sql, function (error, results, fields) {
-                  if (error) throw error;
-                  if(results.length == 0){
-                    //说明该资源为新增，需要插入数据库
-                    let sql = "insert into resources values(?,?,?,?,?,?)";
-                    let params=[null,sub.id, item.title, item.episode, item.link, item.torrent];
-                    connection.query(sql,params,(err,result)=>{
-                        if (err) {
-                            console.error("新增失败" + err.message);
-                            return;
-                        }
-                        console.log("新增成功", item.title);
-                    });
-
-                    //判断是否为新的一集，并更新番剧列表
-                    if(item.episode > LastIndex){
-                        LastIndex = item.episode
-                        let sql = `UPDATE bangumi SET last_index = ? WHERE id = ?`;
-                        let data = [item.episode, sub.id];
-                        connection.query(sql, data, (error, results, fields) => {
-                            if (error){
-                                return console.error(error.message);
+    
+        axios.get('https://mikanani.me/RSS/MyBangumi?token=g7fiCQpq5lEeUkZ3gSn27LgehEcZ3larcTuNwFb5LUw%3d')
+        .then(res => {
+            var result = convert.xml2json(res.data, {compact: true, spaces: 4});
+            let rss = JSON.parse(result)
+            let bangumi = rss.rss.channel.item;        
+            //填充订阅资源列表
+            subscribes.forEach((sub, index) => {
+                //可能存在多个名称，所以每个名称都匹配一遍
+                sub.title.forEach(name => {
+                    bangumi.forEach(anim => {
+                        if(anim.guid._text.lastIndexOf(name) >= 0){
+                            //查询出这是第几集
+                            let episodeIndex = String(anim.guid._text).search(/\[[0-9][0-9]\]/) == -1 ? //[05]
+                                    (String(anim.guid._text).search(/\【[0-9][0-9]\】/) == -1 ? //【05】
+                                        (String(anim.guid._text).search(/\s[0-9][0-9]\s/) == -1 ? //  05  
+                                        -1:String(anim.guid._text).search(/\s[0-9][0-9]\s/))
+                                    :String(anim.guid._text).search(/\【[0-9][0-9]\】/) )
+                                :String(anim.guid._text).search(/\[[0-9][0-9]\]/)
+                            if(episodeIndex != -1){
+                                let episode = parseInt(anim.guid._text.slice(episodeIndex+1, episodeIndex+3))
+                                subscribes[index].source.push({
+                                    bangumiId: sub.id,
+                                    title: anim.guid._text,
+                                    episode: episode,
+                                    link: anim.link._text,
+                                    torrent: anim.enclosure._attributes.url
+                                })
+                                
                             }
-                            console.log('剧集数已更新:', item.episode);
+                        }
+                    });
+                })
+            })
+    
+            //按集数倒序排列
+            subscribes.forEach(sub => {
+                sub.source.sort(function(a, b) {
+                    return b.episode - a.episode
+                })
+            });
+    
+            //更新番剧资源
+            let newList = []
+            console.log("subscribes: ", subscribes)
+            subscribes.forEach((sub, index) => {
+                let LastIndex = subscribes[index].lastIndex
+                sub.source.forEach((item, index2) => {
+                    //判断该资源是否已被收录
+                    let sql = `SELECT id from resources where link = '${item.link}'`
+                    connection.query(sql, function (error, results, fields) {
+                      if (error) throw error;
+                      if(results.length == 0){
+                        //说明该资源为新增，需要插入数据库
+                        let sql = "insert into resources values(?,?,?,?,?,?)";
+                        let params=[null,sub.id, item.title, item.episode, item.link, item.torrent];
+                        connection.query(sql,params,(err,result)=>{
+                            if (err) {
+                                console.error("新增失败" + err.message);
+                                return;
+                            }
+                            // console.log("新增成功", item.title);
                         });
-                        //记录哪些番剧这次发生了更新
-                        newList.push({
-                            bangumiId: sub.id,
-                            episode: item.episode
-                        })
-                    }
-                  }
-                });                
-            });            
-        });  
+    
+                        //判断是否为新的一集，并更新番剧列表
+                        if(item.episode > LastIndex){
+                            LastIndex = item.episode
+                            let sql = `UPDATE bangumi SET last_index = ? WHERE id = ?`;
+                            let data = [item.episode, sub.id];
+                            connection.query(sql, data, (error, results, fields) => {
+                                if (error){
+                                    return console.error(error.message);
+                                }
+                            });
+                            //记录哪些番剧这次发生了更新
+                            newList.push(item)
+                            if(index == subscribes.length - 1){
+                                console.log("newList ", newList)           
+                                //     //组织文本
+                                let msg = "【订阅小助手】有番剧更新啦\n"
+                                if(newList.length == 0){
+                                    resove({
+                                        msg: msg,
+                                        hasUpdate: false
+                                    })  
+                                }
+                                // new Promise(function(resove1, reject1){
+                                    
+                                // })
+                                let users = new Set();
+                                newList.forEach((item, index) => {
+                                    msg += "-------------------\n"
+                                    msg += "番剧名称: " + item.title + "\n"
+                                    msg += "种子地址: " + item.torrent + "\n"
 
-        //通知订阅用户
-        noticeAll(newList, connection)
+                                    //查出有哪些用户订阅了该番剧 
+                                    let sql = `select qq from user where subscribes like '%|${item.bangumiId}|%'`                              
+                                    connection.query(sql, data, (error, results, fields) => {
+                                        if (error){
+                                            return console.error(error.message);
+                                        }
+                                        console.log("这些用户 ", results, "订阅了", item.title)
+                                        results.forEach(user => {
+                                            users.add(parseInt(user.qq))
+                                        });
+
+                                        if(index == newList.length-1){
+                                            msg += "-------------------\n"
+                                            resove({
+                                                msg: msg,
+                                                users: Array.from(users),
+                                                hasUpdate: true
+                                            })  
+                                        }
+                                    });
+                                });             
+                            }
+                        }
+                      }
+                    });  
+
+                });  
+            
+            });  
+        })
+        .catch(err => {
+            console.log('Error: ', err.message);
+        });    
     })
-    .catch(err => {
-        console.log('Error: ', err.message);
-    });    
+}
+
+/*查询订阅更新情况
+【订阅小助手】最新番剧资源
+------------------------------
+番剧名称:古见同学有交流障碍症[19]
+下载地址:https://mikanani.me/Download/20220519/20e2db4e41a3a4c5f110428a8c06cfd433d676f9.torrent
+@张大力
+------------------------------
+*/
+function getUpdateInfo(connection, qq){
+    return new Promise(function(resoveOK, reject){    
+        let results = []    
+        new Promise(function(resove, reject){        
+            connection.query(`SELECT * from user where qq = ${qq}`, function (error, user, fields) {
+                if (error) throw error;
+                user = user[0]
+                // console.log("user ", user)
+                // console.log("subscribes ", user.subscribes)
+                //先查出来该用户订阅了哪些番剧
+                let queryBangumiSql = `SELECT * from bangumi where id in (${user.subscribes})`
+                // console.log("queryBangumiSql ", queryBangumiSql)        
+                connection.query(queryBangumiSql, function (error, bangumis, fields) {
+                    if (error) throw error;
+                    // console.log("bangumis ", bangumis)
+                    bangumis.forEach((bangumi, index) => {
+                        let queryResourceSql = `select * from resources where bangumiId = ${bangumi.id} and episode = ${bangumi.last_index}`
+                        // console.log("queryResourceSql ", queryResourceSql)        
+                        connection.query(queryResourceSql, function (error, resource, fields) {
+                            if (error) throw error;
+                            results.push({
+                                title: resource[0].title,
+                                torrent: resource[0].torrent
+                            })
+                            if(index == bangumis.length - 1){
+                                resove(results)
+                            }
+                        });                
+                    });
+                });
+            });
+        }).then(function(results) {        
+            // console.log("results ", results)
+            //组装文本
+            let msg = "【订阅小助手】订阅中的最新番剧\n"
+            results.forEach(item => {
+                msg += "-------------------\n"
+                msg += "番剧名称: " + item.title + "\n"
+                msg += "种子地址: " + item.torrent + "\n"
+                // msg += "-------------------\n"
+            });
+            resoveOK(msg)
+            // console.log("results:", msg)
+
+        })
+    })
 }
 
 /*
@@ -118,7 +213,7 @@ function updateBangumi(bot, connection){
 @张大力@XXX
 ------------------------------
 */
-function noticeAll(list, connection){
+function noticeAll(list, connection, bot){
     console.log("list", list)
     let noticeArry = new Map()
     list.forEach(item => {
@@ -143,4 +238,4 @@ function noticeAll(list, connection){
 }
 
 
-module.exports = {updateBangumi}
+module.exports = {updateBangumi, getUpdateInfo}
--- main.js
+++ main.js
@@ -12,71 +12,98 @@ var con = mysql.createConnection({
   });     
 con.connect();
 
-bangumi.updateBangumi(null, con);
+//更新资源并自动发布更新消息@用户
+// bangumi.updateBangumi(null, con);
 
+// debugger;
+const bot = new Mirai({
+  // host: 'http://budeliao.top:8082',
+  host: 'http://127.0.0.1:8082',
+  verifyKey: 'INITKEYEC6eueUp',
+  qq: 570918181,
+  enableWebsocket: true,
+  wsOnly: true,
+});
 
-// const bot = new Mirai({
-//   host: 'http://127.0.0.1:8082',
-//   verifyKey: 'INITKEYEC6eueUp',
-//   qq: 570918181,
-//   enableWebsocket: true,
-//   wsOnly: true,
-// });
+// auth 认证(*)
+bot.onSignal('authed', () => {
+  console.log(`Authed with session key ${bot.sessionKey}`);
+  bot.verify();
+});
 
-// // auth 认证(*)
-// bot.onSignal('authed', () => {
-//   console.log(`Authed with session key ${bot.sessionKey}`);
-//   bot.verify();
-// });
+// session 校验回调
+bot.onSignal('verified', async () => {
+  console.log(`Verified with session key ${bot.sessionKey}`);
 
-// // session 校验回调
-// bot.onSignal('verified', async () => {
-//   console.log(`Verified with session key ${bot.sessionKey}`);
+  // 获取好友列表，需要等待 session 校验之后 (verified) 才能调用 SDK 中的主动接口
+  const friendList = await bot.getFriendList();
+  console.log(`There are ${friendList.length} friends in bot`);
+});
 
-//   // 获取好友列表，需要等待 session 校验之后 (verified) 才能调用 SDK 中的主动接口
-//   const friendList = await bot.getFriendList();
-//   console.log(`There are ${friendList.length} friends in bot`);
-// });
-
-// // 接受消息,发送消息(*)
-// bot.onMessage(async message => {
-//   const { type, sender, messageChain, reply, quoteReply } = message;
-//   let msg = '';
-//   messageChain.forEach(chain => {
-//     if (chain.type === 'Plain')
-//         msg += Plain.value(chain);       // 从 messageChain 中提取文字内容
-//   });
-//   console.log("msg: ", msg)
+// 接受消息,发送消息(*)
+bot.onMessage(async message => {
+  const { type, sender, messageChain, reply, quoteReply } = message;
+  let msg = '';
+  messageChain.forEach(chain => {
+    if (chain.type === 'Plain')
+        msg += Plain.value(chain);       // 从 messageChain 中提取文字内容
+  });
+  console.log("msg: ", msg)
+  console.log("sender: ", sender)
 
   
-//   if (msg.includes('bgm isUpdated')){
-//     reply('开始查询番剧更新情况');
-//     bangumi.updateBangumi(bot, con);
-//   }
+  if (msg.includes('bgm')){
+    console.log("开始查询番剧更新情况")
+    bangumi.updateBangumi(con).then(function(res){    
+      console.log("res ", res)
+      if(res.hasUpdate){
+        let actions = [
+          Plain(res.msg)
+        ]
+        res.users.forEach(qq => {
+          actions.push(At(qq))
+        });
+        reply(actions, message);
+      }else {
+        reply([Plain("很遗憾，并没有番剧更新捏"), At(sender.id)], message);
+      }
+    })
+  }
 
-//   // 直接回复
-//   if (msg.includes('收到了吗'))
-//     reply('收到了收到了');                          // 或者: bot.reply('收到了收到了', message)
-//   // 引用回复
-//   else if (msg.includes('引用我'))
-//     quoteReply([At(sender.id), Plain('好的')]);     // 或者: bot.quoteReply(messageChain, message)
-//   // 撤回消息
-//   else if (msg.includes('撤回'))
-//     bot.recall(message);
-//   // 发送图片，参数接受图片路径或 Buffer
-//   else if (msg.includes('来张图'))
-//     bot.sendImageMessage("./image.jpg", message);
-// });
+  if(msg.includes('setu')){        
+    reply('别急，你先别急');  
+  }
 
-// /* 开始监听消息(*)
-//  * 'all' - 监听好友和群
-//  * 'friend' - 只监听好友
-//  * 'group' - 只监听群
-//  * 'temp' - 只监听临时会话
-// */
-// bot.listen('all'); // 相当于 bot.listen('friend', 'group', 'temp')
+  if(msg.includes('getUpdate')){    
+    bangumi.getUpdateInfo(con, sender.id).then(function(result){      
+      console.log("getUpdateInfo: ", result)
+      bot.quoteReply([Plain(result)], message); // 引用消息
+    });    
+  }
 
-// // 退出前向 mirai-http-api 发送释放指令(*)
-// process.on('exit', () => {
-//   bot.release();
-// });
+  // 直接回复
+  if (msg.includes('收到了吗'))
+    reply('收到了收到了');                          // 或者: bot.reply('收到了收到了', message)
+  // 引用回复
+  else if (msg.includes('引用我'))
+    quoteReply([At(sender.id), Plain('好的')]);     // 或者: bot.quoteReply(messageChain, message)
+  // 撤回消息
+  else if (msg.includes('撤回'))
+    bot.recall(message);
+  // 发送图片，参数接受图片路径或 Buffer
+  else if (msg.includes('来张图'))
+    bot.sendImageMessage("./image.jpg", message);
+});
+
+/* 开始监听消息(*)
+ * 'all' - 监听好友和群
+ * 'friend' - 只监听好友
+ * 'group' - 只监听群
+ * 'temp' - 只监听临时会话
+*/
+bot.listen('all'); // 相当于 bot.listen('friend', 'group', 'temp')
+
+// 退出前向 mirai-http-api 发送释放指令(*)
+process.on('exit', () => {
+  bot.release();
+});
